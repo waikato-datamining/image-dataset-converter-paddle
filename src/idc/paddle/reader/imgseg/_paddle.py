dddb68a1835bdffce659222f4408e4c2f6cc79b3
Peter Reutemann <fracpete@gmail.com>
2025-09-17 16:00:23 +1200

    fixed has_finished() method

--- src/idc/paddle/reader/imgseg/_paddle.py
+++ src/idc/paddle/reader/imgseg/_paddle.py
@@ -175,4 +175,4 @@ class PaddleImageSegmentationReader(Reader, PlaceholderSupporter):
         :return: True if finished
         :rtype: bool
         """
-        return len(self._inputs) == 0
+        return (self._inputs is not None) and len(self._inputs) == 0
